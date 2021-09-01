1616829741952937d66f936d9f6d1d66da1f0705
Mike van Kleef <mike.vkleef@gmail.com>
2021-09-01 19:03:25 +0200

    added comments

--- scripts/fn_combine_gate_employee.R
+++ scripts/fn_combine_gate_employee.R
@@ -1,9 +1,9 @@
 CombineGateEmployee <- function(gate, employee){
-  # This function combines the gate data with all employees of different contractors
+  # This function combines the gate data with all employees of different contractors to extract the job_function_type of the employee
   #
   # Args:
   # - gate:     data frame with gate data, in the form of output function CleanGateData
-  # - employee: data frame with job_function data, in the form of output function ......
+  # - employee: data frame with employee data, in the form of output function ......
   #
   # Returns:
   # - data: data frame with gate data and employee data
