f923bcc8e1112d6045794f5f3b2d5127cb7082b3
Mike van Kleef <mike.vkleef@gmail.com>
2021-09-07 11:10:12 +0200

    added selection final product gate hours between declaration hours

--- scripts/fn_combine_contractor_gateagg.R
+++ scripts/fn_combine_contractor_gateagg.R
@@ -7,7 +7,7 @@ CombineContractorGateAgg <- function(contractor, gate_agg, employee){
   # - employee    : data frame with employee data from the different contractors, in the form of output function ......
   #
   # Returns:
-  # - uren_controle_person: data frame with contractors data and gate data 
+  # - hours_check_employee_working_day: data frame with contractors data and gate data at the level employee, working day
   #
 
   # remove rows with unknown common_id (XXXX)
@@ -15,25 +15,67 @@ CombineContractorGateAgg <- function(contractor, gate_agg, employee){
   
   # Keep only gate data which matches the employees from the different contractors
   gate_agg <- gate_agg[gate_agg$common_id %in% employee$common_id,]
-    
-
   
-  uren_controle_person  <- contractor %>%
-    full_join(gate_agg, by = c('common_id' = 'common_id', 'date_work' = 'working_day', 'job_function_type' = 'job_function_type')) %>%
+  
+  # Last Date at Work
+  contractor <- contractor %>%
+    group_by(common_id) %>%
+    mutate(
+      first_work_day_contractor         = min(date_work), 
+      last_work_day_contractor          = max(date_work),
+      job_function_type_direct_indirect = max(job_function_type)  # Indirect gaat voor Direct
+    ) %>%
+    select(-c(job_function_type)) %>%
+    ungroup %>% as.data.frame()
+  
+
+  hours_check_employee_working_day  <- contractor %>%
+    full_join(gate_agg, by = c('common_id' = 'common_id', 'date_work' = 'working_day', 'job_function_type_direct_indirect' = 'job_function_type')) %>%
+    
+    # Include only gate data in the period of declaration
+    filter(date_work >= first_work_day_contractor -1 & date_work <= last_work_day_contractor + 1) %>%
     
     mutate(
-      decl_working_hours               = if_else(is.na(decl_working_hours)  ==TRUE, 0, decl_working_hours),
-      bruto_working_hours              = if_else(is.na(bruto_working_hours) ==TRUE, 0, bruto_working_hours),
-      netto_working_hours              = if_else(is.na(netto_working_hours) ==TRUE, 0, netto_working_hours),
+      decl_working_hours               = if_else(is.na(decl_working_hours)           ==TRUE, 0, decl_working_hours),
+      bruto_working_hours              = if_else(is.na(bruto_working_hours)          ==TRUE, 0, bruto_working_hours),
+      tot_correction_early_arrival     = if_else(is.na(tot_correction_early_arrival) ==TRUE, 0, tot_correction_early_arrival),
+      work_break                       = if_else(is.na(work_break)                   ==TRUE, 0, work_break),
+      netto_working_hours              = if_else(is.na(netto_working_hours)          ==TRUE, 0, netto_working_hours),
     ) %>%
     
     mutate(
       delta_declaration_vs_bruto_hours = round(decl_working_hours - bruto_working_hours,2),
       delta_declaration_vs_netto_hours = round(decl_working_hours - netto_working_hours,2)
-    )
-  
+    ) %>%
+
+     select(
+       common_id,
+       full_name,
+       last_name_gate                      = last_name,
+       first_name_gate                     = first_name,
+       contractor_decl,
+       job_function,
+       job_function_type                   = job_function_type_direct_indirect,
+       duplicate_function_type,
+       date_work,
+       decl_total_working_days,
+       decl_working_hours,
+       correction_start_shift_ind,
+       bruto_working_hours,
+       tot_correction_early_arrival,
+       work_break,
+       netto_working_hours,
+       delta_declaration_vs_bruto_hours,
+       delta_declaration_vs_netto_hours,
+       first_clock,
+       last_clock_buiten_site,
+       working_days_without_checkout_correction_ind
+       #remark
+       
+     )
+    
   
     
-return(uren_controle_person)
+return(hours_check_employee_working_day)
   
 }
