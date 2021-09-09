603f0bc60aa543fb27580667d78a2a4923dcf375
Mike van Kleef <mike.vkleef@gmail.com>
2021-09-09 21:53:16 +0200

    rename columns

--- scripts/fn_aggregate_gate.R
+++ scripts/fn_aggregate_gate.R
@@ -28,9 +28,9 @@ AggregateGate <- function(gate, p_shift_start_day, p_shift_start_night, p_shift_
     group_by(common_id, common_id_unique_ind, job_function_type, shift_type, working_day, duplicate_function_type, 
              first_clock, first_clock_time, last_clock, last_clock_buiten_site, deviating_start_shift, number_of_clocks) %>%
     summarise(
-      bruto_working_hours                          = sum(workhours),
-      tot_correction_workhours_no_check_out        = sum(correction_workhours_no_check_out),
-      bruto_working_hours_with_correction          = sum(workhours - correction_workhours_no_check_out),
+      tot_hours_on_site                            = sum(workhours),
+      tot_correction_no_check_out                  = sum(correction_no_check_out),
+      bruto_working_hours                          = sum(workhours - correction_no_check_out),
       tot_correction_early_arrival                 = sum(correction_early_arrival), 
       correction_start_shift_ind                   = max(correction_start_ind, na.rm = TRUE),
       tot_correction_late_departed                 = sum(correction_late_departed), 
@@ -44,9 +44,9 @@ AggregateGate <- function(gate, p_shift_start_day, p_shift_start_night, p_shift_
       
       # Determine Work Break
       work_break            = case_when(
-        bruto_working_hours_with_correction <= p_work_break_min_threshhold                                                                   ~ 0,
-        bruto_working_hours_with_correction > p_work_break_min_threshhold & bruto_working_hours_with_correction <= p_work_break_threshhold   ~ p_work_break_small,
-        TRUE                                                                                                                                 ~ p_work_break_normal,
+        bruto_working_hours <= p_work_break_min_threshhold                                                               ~ 0,
+        bruto_working_hours > p_work_break_min_threshhold & bruto_working_hours <= p_work_break_threshhold               ~ p_work_break_small,
+        TRUE                                                                                                             ~ p_work_break_normal,
         ),
       
       # change_of_dress_time
@@ -56,7 +56,7 @@ AggregateGate <- function(gate, p_shift_start_day, p_shift_start_night, p_shift_
       ),
       
       # Determine Netto Working Hours
-      netto_working_hours          = bruto_working_hours_with_correction - tot_correction_early_arrival - tot_correction_late_departed - work_break - change_of_dress_time
+      netto_working_hours          = bruto_working_hours - tot_correction_early_arrival - tot_correction_late_departed - work_break - change_of_dress_time
       
       ) %>% as.data.frame()
 
@@ -68,36 +68,7 @@ AggregateGate <- function(gate, p_shift_start_day, p_shift_start_night, p_shift_
    cat('number of records:', nrow(gate_agg), '\n')
    cat('number of unique ids:', n_unique_ids, '\n')
    
-  
-#------------------------------------------------------------------------------
-#  CORRECTION START KAN NIET OP AGGRAGATE WANT IEMAND KAN VOOR STARTSHIFT OOK WEER UITKLOKKEN  
-# determine correction_early_arrival
-  # gate_agg$start_dayshift     <- p_shift_start_day    # add shift_start_day
-  # gate_agg$start_nightshift   <- p_shift_start_night  # add shift_start_night
-  # 
-  # gate_agg <- gate_agg %>%
-  #   mutate(
-  #     
-  #     first_clock_date             = format(gate_agg$first_clock, format = "%Y-%m-%d"),
-  # 
-  #     date_check_in_out_correction_agg = case_when(
-  #       
-  #       toupper(job_function_type) == "DIRECT" & shift_type == "dagshift"
-  #       & first_clock_time > (hms(p_shift_start_day) - hours(p_hour)) 
-  #       & first_clock_time < hms(p_shift_start_day)                         ~ as.POSIXct(paste0(first_clock_date, " ", start_dayshift), format = "%Y-%m-%d %H:%M:%S", tz = 'UTC'),
-  #       
-  #       toupper(job_function_type) == "DIRECT" & shift_type == "nachtshift"
-  #       & first_clock_time > (hms(p_shift_start_night) - hours(p_hour)) 
-  #       & first_clock_time < hms(p_shift_start_night)                       ~ as.POSIXct(paste0(first_clock_date, " ", start_nightshift), format = "%Y-%m-%d %H:%M:%S", tz = 'UTC'),
-  #       
-  #       TRUE                                                                ~ first_clock
-  #     ),
-  #     
-  #     correction_early_arrival_agg     =  round(difftime( date_check_in_out_correction_agg, first_clock , units = "hours"),3)   
-  #   ) 
-  # 
-  
-    
+
     
 
 return(gate_agg)
