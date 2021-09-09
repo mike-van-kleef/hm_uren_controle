5f959898755c42fcd130cd447f2fea6d816f6a91
Mike van Kleef <mike.vkleef@gmail.com>
2021-09-09 21:52:56 +0200

    rename columns NAs removed in full_name and contractor_decl Add columns double_decl_same_day and double_name_same_common_id

--- scripts/fn_aggregate_employee.R
+++ scripts/fn_aggregate_employee.R
@@ -11,37 +11,52 @@ EmployeeCheckAgg <- function(hours_check_employee_working_day){
   
 
   
-  # employee check
-  employee_check <- hours_check_employee_working_day %>%
+  # employee with known common id. Replace NAs from attributes
+  employee_check_known_id <- hours_check_employee_working_day %>%
+    filter(common_id != 'XXXX') %>%
+    group_by(common_id) %>%
+    mutate(
+      full_name                             = max(full_name, na.rm = TRUE),
+      contractor_decl                       = max(contractor_decl, na.rm = TRUE)
+    ) %>% ungroup() %>% as.data.frame()
+    
+  
+  # employee with unknown common id. 
+  employee_check_unknown_id <- hours_check_employee_working_day %>%
+    filter(common_id == 'XXXX') 
+  
+  
+  # bind data frames  
+  employee_check = rbind(employee_check_known_id,employee_check_unknown_id)
+
+  employee_check <- employee_check %>%    
     group_by(common_id, full_name, job_function_type, contractor_decl) %>%
     summarise(
-      #contractor                              = max(if_else(is.na(contractor) ==TRUE, "",contractor)),
-      job_function_type                       = max(if_else(is.na(job_function_type) ==TRUE, "",job_function_type)),
+      double_decl_same_day                    = max(double_decl_same_day, na.rm = TRUE),
+      double_name_same_common_id              = max(double_name_same_common_id, na.rm = TRUE),
       tot_decl_working_days                   = sum(if_else(decl_working_hours > 0, 1, 0)),
       tot_decl_working_hours                  = sum(decl_working_hours),
       tot_working_days                        = sum(if_else(bruto_working_hours > 0, 1, 0)),
-      tot_bruto_working_hours                 = sum(bruto_working_hours),
-      tot_correction_workhours_no_check_out   = sum(tot_correction_workhours_no_check_out),
-      tot_bruto_working_hours_with_correction = sum(bruto_working_hours_with_correction),
+      tot_hours_on_site                       = sum(tot_hours_on_site),
+      tot_correction_no_check_out             = sum(tot_correction_no_check_out),
+      bruto_working_hours                     = sum(bruto_working_hours),
       tot_correction_early_arrival            = sum(tot_correction_early_arrival),
       tot_correction_late_departed            = sum(tot_correction_late_departed),
       tot_work_break                          = sum(work_break),
       tot_change_of_dress_time                = sum(change_of_dress_time),
       tot_netto_working_hours                 = sum(netto_working_hours),
+      tot_netto_working_hours_cor             = sum(netto_working_hours_cor),
       tot_delta_decl_vs_bruto_hours           = sum(delta_decl_vs_bruto_hours),
-      tot_delta_decl_vs_netto_hours           = sum(delta_decl_vs_netto_hours)
+      tot_delta_decl_vs_netto_hours           = sum(delta_decl_vs_netto_hours),
+      tot_delta_decl_vs_netto_hours_cor       = sum(delta_decl_vs_netto_hours_cor)
     ) %>% 
-    group_by(common_id) %>%
-    mutate(
-      duplicated_person                     = n_distinct(full_name)
-    ) %>% ungroup() %>% 
     mutate(
       remark = case_when(
         common_id               == 'XXXX'                                        ~ 'Employee not found',
         tot_netto_working_hours == 0                                             ~ 'Employee found - no gate clocking for period',
         TRUE                                                                     ~ ''
-      )
-    ) %>% as.data.frame()
+        )
+      ) %>% as.data.frame()
   
   
   
