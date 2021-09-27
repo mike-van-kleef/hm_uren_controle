89223d52c4d20967a1e2b47073b0a771266bc1ac
Mike van Kleef <mike.vkleef@gmail.com>
2021-09-27 21:06:08 +0200

    clean mammoet data

--- scripts/fn_clean_mammoet_data.R
+++ scripts/fn_clean_mammoet_data.R
@@ -14,15 +14,20 @@ CleanMammoetData <- function(data, job_function = ""){
   
 # sort data by common_id, week_nr
   data <- data %>%
+    filter(labour_equipment_ind == 'Labour Costs') %>%
     arrange(common_id, week_nr, cost_center_code)
 
-# delete rows without common_id
-  print(data %>% filter(is.na(common_id) == TRUE))
-  data <- data %>% filter(is.na(common_id) == FALSE)
 
 # NOG AANPASSEN IN BESTAND
   data <- data %>%
     mutate(
+      common_id = case_when(
+        is.na(common_id) == TRUE              ~ 'XXXX',
+        substr(common_id,1,1) == '?'          ~ 'XXXX',
+        TRUE                                  ~ toupper(common_id)
+      ),
+      
+      
       full_name = case_when(
         full_name == 'Bal, A,.'               ~ 'Bal, A.',
         full_name == 'Vette N de.'            ~ 'Vette, N de.',
@@ -30,8 +35,17 @@ CleanMammoetData <- function(data, job_function = ""){
         full_name == 'Bie, R de.'             ~ 'Bie, R. de',
         full_name == 'Brooks, A.'             ~ 'Brooks, A',
         TRUE                                  ~ full_name
-      ))
-        
+        ),
+      
+      week_nr = sub("week *", "", tolower(week_nr))
+      )
+
+# delete rows without common_id
+  cat('\n','Employee without common_id', '\n')
+  teller <- nrow(data[is.na(data$common_id) == TRUE,])
+  if(teller > 0 ){
+  print(data %>% filter(is.na(common_id) == TRUE))}
+
     
 #----------------------------------------------------------------------------------------
 # pivot columns with start, eind and pauze to rows  -------------------------------------
@@ -113,125 +127,76 @@ CleanMammoetData <- function(data, job_function = ""){
      decl_time_eind_dummy      = if_else(decl_time_eind < decl_time_start, decl_time_eind + 3600*24, decl_time_eind),
      decl_time_pauze           = if_else(is.na(decl_time_pauze) == TRUE, 0 , minute(hms(decl_time_pauze)) / 60),
      decl_working_hours_bruto  = as.numeric(difftime(decl_time_eind_dummy, decl_time_start, unit = 'hours')),
-     decl_working_hours        = decl_working_hours_bruto - decl_time_pauze
-   )  %>%
-   group_by(common_id, full_name, week_nr,day) %>%
+     decl_working_hours        = decl_working_hours_bruto - decl_time_pauze,
+     
+     day_nr                    = case_when(
+       day == 'ma' ~ 1,
+       day == 'di' ~ 2,
+       day == 'wo' ~ 3,
+       day == 'do' ~ 4,
+       day == 'vr' ~ 5,
+       day == 'za' ~ 6,
+       day == 'zo' ~ 7
+       
+     ) 
+   ) 
+ 
+ data_samen_agg <- data_samen %>%
+   group_by(common_id, full_name, week_nr,day, day_nr) %>%
    summarise(
      double_decl_same_day = n(),
+     decl_total_working_days = -1,
      decl_working_hours   = sum(decl_working_hours)
    ) %>% as.data.frame()
    
  
- # check
- x = plyr::count(data_samen,c('common_id','week_nr','cost_center_code','day'))
- x[x$freq>1,]
+# combine data frame with data_all
+  decl <- data_samen_agg %>%
+   inner_join(data_all,  by = c('common_id','full_name','week_nr')) 
 
- 
-
-# combine data frame
- data_all2 <- data_all %>%
-   inner_join(data_samen,  by = c('common_id','week_nr')) %>%
-
-
-
-
- mutate(
-  decl_time_start     = format(decl_time_start,  format = "%H:%M:%S"),
-) %>%
-  
-
-    
-  
-# remove rows with no declaration for working hours    
-  data <- data[is.na(data$decl_working_hours) == FALSE,]
-
-
-# clean job function
-  job_function <- job_function %>%
+# No correction on early arrival and late departed. Hereby all job_function_type are indirect
+# Add attributes
+  decl <- decl %>%
     mutate(
-      job_function      = tolower(trimws(job_function)),
-      job_function_type = trimws(job_function_type)
-    )
-  
-  
-  
-# Add job function type (direct vs indirect)    
-  data <- left_join(data, job_function, by = c('job_function' = 'job_function'))
-
-# remove columns
-  data <- data %>%
-    select(-c(decl_night_working_days,decl_night_working_hours,decl_night_working_hours_critic,
-              decl_day_working_days,decl_day_working_hours,decl_day_working_hours_critic,decl_total_working_hours_critic
-               )
-           )
-
-# Name contractor  
-  data$contractor_decl = 'Bilfinger Maintenance'
-  
-  # aantal unieke common_id - full_name
-  #nrow(unique(data[,c('common_id','full_name','job_function','site','night_shift')]))      
-
-# NOG AANPASSEN IN BESTAND
-  data <- data %>%
-    mutate(
-      
-      common_id = case_when(
-        full_name == 'Rodriquez, Pieter Jose Frederik Jan'~ '27021967RODR',
-        full_name == 'Wisniewski, Marcin'                 ~ '11041981WISN',
-        full_name == 'Mijnhals, Wesley'                   ~ '16101991MIJN',
-        TRUE                                              ~  common_id 
-        ),
-      
-      job_function = case_when(
-        common_id == '01111978ORCU'                       ~ 'foreman',
-        TRUE                                              ~ job_function
-       )
-    )
-
-    
-# Employee has two or more declaration on same day
-  double_decl = plyr::count(data[,c('common_id','full_name','date_work')])
-  print(double_decl[double_decl$freq > 1,])
-  cat("\n")
-  
-# print total number of double declaration
-  cat('number of double declaration on same day:', sum(double_decl[double_decl$freq > 1,]$freq - 1), '\n')
-  cat('\n','Employee with different name for same common_id', '\n')
-  
-# Employee has two names with same common_id
-  employee = unique(data[,c('common_id','full_name')])
-  id_not_unique = plyr::count(employee[,c('common_id')]) 
-  id_not_unique = id_not_unique[id_not_unique$freq >1,]
-  print(employee[employee$common_id %in% id_not_unique$x & employee$common_i!= 'XXXX', ])    
-  cat("\n")
-
-  data <- data %>% 
-    
-    # double declaration same day
-    group_by(common_id, full_name, date_work) %>%
-    mutate(
-      double_decl_same_day = if_else( n() >1, 'Ja', 'Nee'),
-    ) %>% ungroup() %>%
-    
+      job_function_type = 'Indirect',
+      job_function      = 'Onbekend',
+      date_work         = if_else(day == 'zo',as.Date(paste(2021, week_nr, day_nr, sep="-"), "%Y-%W-%u") + 7, as.Date(paste(2021, week_nr, day_nr, sep="-"), "%Y-%W-%u")),
+      contractor_decl   = 'Mammoet'
+    ) %>%
     # double name same common_id
     group_by(common_id) %>%
     mutate(
       double_name_same_common_id = n_distinct(full_name)
     ) %>%
-    ungroup() %>% as.data.frame()
+    ungroup() %>% 
+    arrange(common_id, full_name, week_nr, day_nr) %>% as.data.frame()
+
+
+
+
+    
+# Employee has two or more declaration on same day
+  cat("\n")
+  cat("Employee has two or more declaration on same day","\n")
+  print(decl[decl$double_decl_same_day > 1,])
+  cat("\n")
+  
+# print total number of double declaration
+  cat('number of double declaration on same day:', sum(decl[decl$double_decl_same_day > 1,]$double_decl_same_day -1), '\n')
   
   
-# Aggregate to employee and date at work  
-  data <- data %>%
-    group_by(common_id, full_name, date_work, firma, night_shift, site, assignment, land, job_function, job_function_type, contractor_decl, double_decl_same_day, double_name_same_common_id) %>%
-    summarise(
-      tarif                    = max(tarif),
-      decl_total_working_days  = sum(decl_total_working_days),
-      decl_total_working_hours = sum(decl_total_working_hours),
-      decl_working_hours       = sum(decl_working_hours)
-    ) %>% as.data.frame()
   
-return(data)  
+# Employee has two names with same common_id
+  cat('\n','Employee with different name for same common_id', '\n')
+  employee = unique(decl[,c('common_id','full_name')])
+  id_not_unique = plyr::count(employee[,c('common_id')]) 
+  id_not_unique = id_not_unique[id_not_unique$freq >1,]
+  print(employee[employee$common_id %in% id_not_unique$x & employee$common_i!= 'XXXX', ])    
+  cat("\n")
+
+  
+  
+return(decl)  
   
 }
   
