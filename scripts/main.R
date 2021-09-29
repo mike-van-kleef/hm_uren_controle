85f3d2f05dbc2f303ab9b9dcc6f8caa029015111
Mike van Kleef <mike.vkleef@gmail.com>
2021-09-29 18:00:44 +0200

    added result employee and contractor added bis

--- scripts/main.R
+++ scripts/main.R
@@ -13,7 +13,8 @@
 # Parameters
 p_select_period              <- TRUE                                    # specifies whether data is selected over a period of time
 p_period_start               <- as.Date('2021-03-14')                   # specifies the start date of the period
-p_period_end                 <- as.Date('2021-06-02')                   # specifies the end date of the period
+p_period_end                 <- as.Date('2021-06-02')                   # specifies the end date of the period (30 mei 2021 + 2 days for marge)
+#p_period_end                 <- as.Date('2021-07-11')                   # specifies the end date of the period for BIS (09 july 2021 + 2 days for marge)
 p_workday_split              <- 5                                       # specifies the split. work hours for the split are counted on the previous day
 p_shift_start_day            <- format("07:00:00", format = "%H:%M:%S") # specifies the start of day shift
 p_shift_start_night          <- format("18:00:00", format = "%H:%M:%S") # specifies the start of night shift
@@ -97,6 +98,11 @@ library(lubridate)
   file_name               <- paste0(file_map,"Mammoet.xlsx")
   df.mammoet_staging      <- import_files(p.meta = TRUE, filemeta = meta_file, filename = file_name, sheet.nr =1, skip_rows = 0)
   
+  file_map                <-  'data/raw/contractors/'
+  meta_file               <- paste0(file_map,"meta_bis.xlsx")
+  file_name               <- paste0(file_map,"BIS.xlsx")
+  df.bis_staging          <- import_files(p.meta = TRUE, filemeta = meta_file, filename = file_name, sheet.nr =1, skip_rows = 3)
+  
 # Clean Data   ----------------------------------------------------------------      
 # clean gate data
   df.gate_clean             <- CleanGateData(data = df.gate_staging, site = df.site_staging, p_workday_split = p_workday_split,
@@ -106,23 +112,26 @@ library(lubridate)
   df.bilfinger_clean        <- CleanBilfingerData(data = df.bf_staging, job_function = df.job_function_bf_staging)  
 
 # clean Mourik data
-  df.mourik_pers_clean      <- CleanMourikData(data = df.mourik_pers_staging, job_function = df.job_function_mourik_staging, p_department = 'Mourik_Personeel')  
-  df.mourik_mint_clean      <- CleanMourikData(data = df.mourik_mint_staging, job_function = df.job_function_mourik_staging, p_department = 'Mourik_International' )  
+  # df.mourik_pers_clean      <- CleanMourikData(data = df.mourik_pers_staging, job_function = df.job_function_mourik_staging, p_department = 'Mourik_Personeel')  
+  # df.mourik_mint_clean      <- CleanMourikData(data = df.mourik_mint_staging, job_function = df.job_function_mourik_staging, p_department = 'Mourik_International' )  
 
 # combine both data frames and aggregate 
-  df.mourik_clean           <- CombineMourikData(pers = df.mourik_pers_clean, mint = df.mourik_mint_clean)
+  # df.mourik_clean           <- CombineMourikData(pers = df.mourik_pers_clean, mint = df.mourik_mint_clean)
 
   
 # clean Mammoet data  
-  df.mammoet_clean        <- CleanMammoetData(data = df.mammoet_staging)    
+  df.mammoet_clean          <- CleanMammoetData(data = df.mammoet_staging)    
 
-  
+
+# clean BIS data    
+  df.bis_clean              <- CleanBisData(data = df.bis_staging)    
       
 # select data for given time horizon. Only if (p_select_period == TRUE
   if (p_select_period == TRUE){
     df.bilfinger_clean      <- df.bilfinger_clean[df.bilfinger_clean$date_work >= p_period_start & df.bilfinger_clean$date_work <= p_period_end,]
-    df.mourik_clean         <- df.mourik_clean[df.mourik_clean$date_work >= p_period_start & df.mourik_clean$date_work <= p_period_end,]
-    df.mammoet_clean        <- df.mammoet_clean[df.mammoet_clean$date_work >= p_period_start & df.mammoet_clean$date_work <= p_period_end,]
+    #df.mourik_clean         <- df.mourik_clean[df.mourik_clean$date_work       >= p_period_start & df.mourik_clean$date_work    <= p_period_end,]
+    df.mammoet_clean        <- df.mammoet_clean[df.mammoet_clean$date_work     >= p_period_start & df.mammoet_clean$date_work   <= p_period_end,]
+    df.bis_clean            <- df.bis_clean[df.bis_clean$date_work     >= p_period_start & df.bis_clean$date_work   <= p_period_end,]
   }
 
 
@@ -132,6 +141,7 @@ library(lubridate)
   #df.contractor            <- CombineContractors(bilfinger = df.bilfinger_clean, mourik = df.mourik_clean)
   df.contractor            <- df.bilfinger_clean
   #df.contractor            <- df.mammoet_clean
+  #df.contractor            <- df.bis_clean
   
 
 # select employees
@@ -163,12 +173,12 @@ library(lubridate)
 
   
 # combine gate data with contractors 
-  df_agg.hours_check_employee_working_day <- CombineContractorGateAgg( contractor = df.contractor, gate_agg = df_agg.gate, employee = df.employee)  
+  df_agg.hours_check_employee_working_day   <- CombineContractorGateAgg( contractor = df.contractor, gate_agg = df_agg.gate, employee = df.employee)  
   
   
 # aggregate to level employee
 # Bilfinger has 628 unique common_ids. 1 common_id with a double full_name and common_id = XXXX with 7 full_names. Together 636 unique common_id - full_names
-  df_agg.hours_check_employee             <- EmployeeCheckAgg(hours_check_employee_working_day = df_agg.hours_check_employee_working_day)  
+  df_agg.hours_check_employee               <- EmployeeCheckAgg(hours_check_employee_working_day = df_agg.hours_check_employee_working_day)  
   
   
 # prepare view for the audit of gate data
@@ -177,40 +187,58 @@ library(lubridate)
 
 # prepare view to level contractor
   df_agg.hours_check_contractor             <- ContractorCheckAgg(employee = df_agg.hours_check_employee)
- 
+
+  
+# prepare overview with employees and contractors according to the gate data
+  employee_contractor_gate                  <- EmployeeContractorFromGate(gate = df.gate_clean)
+  employee_gate                             <- employee_contractor_gate$employee
+  contractor_gate                           <- employee_contractor_gate$contractor_agg
   
 # export files --------------------------------------------------------------------------------------------------------------------------------------
   
 # path results
-  filepath <- "./results/"
+  filepath   <- "./results/"
+  contractor <- unique(df_agg.hours_check_contractor[1:1,])
     
 # export urencontrole_werkdag 
-  filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_urencontrole_werkdag.csv')
+  filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_urencontrole_werkdag_',contractor,'.csv')
   write.csv2(df_agg.hours_check_employee_working_day, paste0(filepath,filename), row.names = FALSE)      
         
 # export urencontrole_medewerker  
-  filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_urencontrole_medewerker.csv')
+  filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_urencontrole_medewerker_',contractor,'.csv')
   write.csv2(df_agg.hours_check_employee, paste0(filepath,filename), row.names = FALSE)        
   
 # export urencontrole_contractor
-  contractor = unique(df_agg.hours_check_contractor[1:1,])
   filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_urencontrole_contractor_',contractor,'.csv')
   write.csv2(df_agg.hours_check_contractor, paste0(filepath,filename), row.names = TRUE)          
 
 # export audit_view_gate_Bilfinger_Maintenance  
   filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_audit_view_gate_Bilfinger_Maintenance.csv')
   write.csv2(list.audit_gate$Bilfinger_Maintenance, paste0(filepath,filename), row.names = FALSE)        
-  
 
 # export audit_view_gate_Mammoet
   filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_audit_view_gate_Mammoet.csv')
   write.csv2(list.audit_gate$Mammoet, paste0(filepath,filename), row.names = FALSE)        
   
+# export audit_view_gate_Bis
+  filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_audit_view_gate_BIS.csv')
+  write.csv2(list.audit_gate$BIS, paste0(filepath,filename), row.names = FALSE)     
+  
 
 # export audit_view_gate_all
   filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_audit_view_gate_all.csv')
-  write.csv2(list.audit_gate$All, paste0(filepath,filename))        
+  write.csv2(list.audit_gate$All, paste0(filepath,filename))  
   
+#   
+# # export employee_gate
+#   filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_employee_gate.csv')
+#   write.csv2(employee_gate, paste0(filepath,filename), row.names = FALSE)        
+#   
+# 
+# # export contractor_gate
+#   filename <- paste0(format(Sys.Date(),'%Y%m%d'),'_contractor_gate.csv')
+#   write.csv2(contractor_gate, paste0(filepath,filename), row.names = FALSE)             
+#   
     
 # test
   med = unique(df.bf_staging[,c('common_id','job_function')])
@@ -222,7 +250,8 @@ library(lubridate)
   df.bf_staging[df.bf_staging$Code_ID == '03081988AAZZ',]
   
   #m =unique(df.bilfinger_clean[,c('full_name','firma','common_id','decl_total_working_hours','tarif')])
-  m =unique(df.mammoet_clean[,c('full_name','common_id')])
+  #m =unique(df.mammoet_clean[,c('full_name','common_id')])
+  m =unique(df.bis_clean[,c('full_name','common_id')])
   m = m[is.na(m$full_name) == FALSE,]
   p = unique(df.gate_clean[,c('common_id','first_name','last_name','contractor','date_birth')])
 
@@ -239,6 +268,7 @@ library(lubridate)
 # write.csv2(z2, "analyse/Bilfinger_unknown_persons.csv")  
 # write.csv2(z2, "Mourik_unknown_persons.csv")  
 # write.csv2(z2, "Mammoet_unknown_persons.csv")  
+# write.csv2(z2, "BIS_unknown_persons.csv")  
 
 # write.csv2(df_agg.hours_check_employee, "results/urencontrole_medewerker.csv")      
 # write.csv2(list.audit_gate$Bilfinger_Maintenance, "results/audit_view_gate_Bilfinger_Maintenance.csv")      
