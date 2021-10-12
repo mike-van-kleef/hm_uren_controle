c3b71b199128e8103e3a76ee17e82962452e922e
Mike van Kleef <mike.vkleef@gmail.com>
2021-10-12 17:16:24 +0200

    Added Mammoet and Mourik for netto_cor_ind = 4

--- scripts/fn_combine_contractor_gateagg.R
+++ scripts/fn_combine_contractor_gateagg.R
@@ -158,7 +158,7 @@ CombineContractorGateAgg <- function(contractor, gate_agg, employee){
       netto_working_hours_cor = case_when(
          netto_cor_ind %in% c(1,2)                                                            ~ 0,
          netto_cor_ind ==   3                                                                 ~ decl_working_hours,
-         netto_cor_ind ==   4 & contractor_decl %in% c('BIS','Mammoet')                       ~ decl_working_hours,
+         netto_cor_ind ==   4 & contractor_decl %in% c('BIS','Mammoet','Mourik')              ~ decl_working_hours,
          TRUE                                                                                 ~ netto_working_hours
       ),
       
